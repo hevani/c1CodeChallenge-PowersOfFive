704d71f949180a3041ac7b5151009683ecd86612
hevani <hayagreev.evani@gmail.com>
2017-02-23 07:23:28 -0500

    Update Solution.java
    updated code

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,19 +1,30 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
 public class Solution {
-
-
     public Solution(){
     }
 
-    public int getMin(String s){
-
-    }
-
-
-
+    public synchronized int getMin (String s ){
+		long [] n = new long [s.length() + 1] ;
+		n[0] = 0;	
+		for (int i = 1 ; i <= s.length() ;++i) {
+			n[i] = Integer.MAX_VALUE;
+			for(int j=i;j>=1;--j) {
+			 if (s.charAt(j - 1) == '0'){
+				 continue ;
+			 }
+			 long num = Long.parseLong(s.substring(j - 1, i), 2) ;				 
+			 if (isPowerOfFive(num)) {
+				 n[i] = Math.min(n[i], n[j - 1] + 1) ;				 
+			 }			 
+		 }
+		}								
+		return n[s.length()] == Integer.MAX_VALUE ? -1 : (int)n[s.length()] ;
+	}
+	
+	private boolean isPowerOfFive(long val){
+		if (val == 0) {
+			return false ;  
+		}
+		int n = (int)(Math.log(val) / Math.log(5));
+		return Math.pow(5, n) == val;
+	}
 }
